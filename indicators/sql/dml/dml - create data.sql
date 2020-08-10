fc234bd138e7049fee67e36108fe08bd7045b39f
ansegura7 <seguraandres7@gmail.com>
2020-08-10 15:38:37 -0500

    Update SQL
    - Updated DML SQL queries.

--- indicators/sql/dml/dml - create data.sql
+++ indicators/sql/dml/dml - create data.sql
@@ -4,16 +4,16 @@ SELECT [indicator],[date],[value]
   FROM 
 	  (SELECT [indicator],[date],SUM([value]) AS [value]
 	     FROM [dbo].[indicator_by_departament]
-		WHERE [indicator] = 'MM'
+		WHERE [indicator] = 'TB'
 	    GROUP BY [indicator],[date]) AS t
  ORDER BY [date];
 GO
 
-SELECT [date],[MM],[IM]
+SELECT [date],[MM],[IM],[TB]
    FROM 
 	  (SELECT [indicator],[date],SUM([value]) AS [value]
 	     FROM [dbo].[indicator_by_departament]
 	    GROUP BY [indicator],[date]) AS t
- PIVOT (SUM([value]) FOR [indicator] IN ([MM],[IM])) AS p
+ PIVOT (SUM([value]) FOR [indicator] IN ([MM],[IM],[TB])) AS p
  ORDER BY [date];
 GO
