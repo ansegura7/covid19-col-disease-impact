dc5ad88a9fa1832b152e29072fc68420a4b2acfa
ansegura7 <seguraandres7@gmail.com>
2020-09-01 09:35:07 -0500

    Update SQL
    - Added a new DML query
    - Updated several DML queries
    - Updated DDL command

--- predictive-analytics/sql/ddl/ddl - create main table.sql
+++ predictive-analytics/sql/ddl/ddl - create main table.sql
@@ -1,14 +1,14 @@
 USE [AC_C19_DATA]
 GO
 
-/****** Object:  Table [dbo].[indicator_by_departament]    Script Date: 8/10/2020 3:46:08 PM ******/
+/****** Object:  Table [dbo].[indicator_data]    Script Date: 9/1/2020 9:29:53 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[indicator_by_departament](
+CREATE TABLE [dbo].[indicator_data](
 	[indicator] [nvarchar](50) NULL,
 	[sub_indicator] [nvarchar](250) NULL,
 	[entity] [nvarchar](50) NULL,
--- predictive-analytics/sql/dml/dml - generate data.sql
+++ predictive-analytics/sql/dml/dml - generate data.sql
@@ -5,7 +5,7 @@ GO
 SELECT [date], YEAR([date]) AS [year], MONTH([date]) AS [month], DATEPART(WEEK, [date]) AS [week], [value]
   FROM 
 	  (SELECT [indicator],[date],SUM([value]) AS [value]
-	     FROM [dbo].[indicator_by_departament]
+	     FROM [dbo].[indicator_data]
 		WHERE [indicator] = 'TB'
 		 -- AND [department] = 'CHOCO'
 	    GROUP BY [indicator],[date]) AS t
@@ -16,7 +16,7 @@ GO
 SELECT [date],[MM],[IM],[TB]
    FROM 
 	  (SELECT [indicator],[date],SUM([value]) AS [value]
-	     FROM [dbo].[indicator_by_departament]
+	     FROM [dbo].[indicator_data]
 	    GROUP BY [indicator],[date]) AS t
  PIVOT (SUM([value]) FOR [indicator] IN ([MM],[IM],[TB])) AS p
  ORDER BY [date];
