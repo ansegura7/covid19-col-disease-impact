70e93e55815a4e633b3ae2bd9c267ba77de7e2f6
ansegura7 <seguraandres7@gmail.com>
2020-09-16 10:13:49 -0500

    Update SQL
    - Updated Data Quality and Generate Data scripts.

--- solution/sql/dml/dml - generate data.sql
+++ solution/sql/dml/dml - generate data.sql
@@ -4,7 +4,7 @@ GO
 -- Generate for the country
 SELECT [date], [year], MONTH([date]) AS [month], [week], SUM([value]) AS [value]
   FROM [dbo].[indicator_data]
- WHERE [indicator] = 'SA'
+ WHERE [indicator] = 'EMM'
    AND [year] >= 2017
  GROUP BY [date], [year], MONTH([date]), [week]
  ORDER BY [date];
@@ -13,7 +13,7 @@ GO
 -- Generate by departments
 SELECT [date], [department], [year], MONTH([date]) AS [month], [week], SUM([value]) AS [value]
   FROM [dbo].[indicator_data]
- WHERE [indicator] = 'SA'
+ WHERE [indicator] = 'EMM'
    AND [year] >= 2017
    AND [department] NOT IN ('EXTERIOR', 'PROCEDENCIA DESCONOCIDA')
  GROUP BY [date], [department], [year], MONTH([date]), [week]
