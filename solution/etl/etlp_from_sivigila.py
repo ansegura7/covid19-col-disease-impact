1ac4f8ecacf27807b446933cc37585e8a3903070
ansegura7 <seguraandres7@gmail.com>
2020-09-18 11:50:29 -0500

    Update SQL
    - Updated DML queries
    - ETL process inserting command updated.

--- solution/etl/etlp_from_sivigila.py
+++ solution/etl/etlp_from_sivigila.py
@@ -76,8 +76,8 @@ def db_save_data(db_login, data_list):
         
         # Insert many rows
         query = '''
-                    INSERT INTO [dbo].[indicator_data]
-                           ([indicator],[sub_indicator],[entity],[department],[year],[week],[value])
+                    INSERT INTO [dbo].[events_data]
+                           ([event],[sub_event],[entity],[department],[year],[week],[value])
                     VALUES (?, ?, ?, ?, ?, ?, ?);
                 '''
         
--- solution/sql/dml/dml - generate data.sql
+++ solution/sql/dml/dml - generate data.sql
@@ -3,8 +3,8 @@ GO
 
 -- Generate for the country
 SELECT [date], [year], MONTH([date]) AS [month], [week], SUM([value]) AS [value]
-  FROM [dbo].[indicator_data]
- WHERE [indicator] = 'EMM'
+  FROM [dbo].[events_data]
+ WHERE [event] = 'EMM'
    AND [year] >= 2017
  GROUP BY [date], [year], MONTH([date]), [week]
  ORDER BY [date];
@@ -12,8 +12,8 @@ GO
 
 -- Generate by departments
 SELECT [date], [department], [year], MONTH([date]) AS [month], [week], SUM([value]) AS [value]
-  FROM [dbo].[indicator_data]
- WHERE [indicator] = 'EMM'
+  FROM [dbo].[events_data]
+ WHERE [event] = 'EMM'
    AND [year] >= 2017
    AND [department] NOT IN ('EXTERIOR', 'PROCEDENCIA DESCONOCIDA')
  GROUP BY [date], [department], [year], MONTH([date]), [week]
